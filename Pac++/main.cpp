7eefeacb81cbbe6bb99c4e5cbdbb365b1825ae5b
Jessen Forbush <for21011@byui.edu>
2025-05-29 17:28:33 -0600

    Initial Commit

---
+++ Pac++/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Pac++
+//
+//  Created by Jessen Forbush on 5/29/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   std::cout << "Hello, World!\n";
+   return 0;
+}
